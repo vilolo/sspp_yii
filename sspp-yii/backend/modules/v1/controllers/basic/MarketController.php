6d92366e32837b05d53a28cae3e8544d6c422e26
unknown <you@example.com>
2020-11-24 00:28:53 +0800

    update

--- sspp-yii/backend/modules/v1/controllers/basic/MarketController.php
+++ sspp-yii/backend/modules/v1/controllers/basic/MarketController.php
@@ -16,12 +16,53 @@ class MarketController extends BackendBaseController
 {
     public function actionIndex()
     {
-        $itemBaseUrl = 'https://shopee.com.my/';
+        //https://shopee.com.my/api/v2/search_items/?by=sales&keyword=anime&limit=50&newest=0&order=desc&page_type=search&version=2
+        //https://xiapi.xiapibuy.com/api/v2/search_items/?by=sales&keyword=文具&limit=50&newest=0&order=desc&page_type=search&version=2
+        //https://th.xiapibuy.com/api/v2/search_items/?by=sales&keyword=bag&limit=50&newest=0&order=desc&page_type=search&version=2
+        //https://br.xiapibuy.com/api/v2/search_items/?by=sales&keyword=bag&limit=50&newest=0&order=desc&page_type=search&version=2
+
+//        $itemBaseUrl = 'https://shopee.com.my/';
+
+        $urlList = [
+            'my' => 'https://shopee.com.my/',
+            'tw' => 'https://xiapi.xiapibuy.com/',
+            'th' => 'https://th.xiapibuy.com/',
+            'br' => 'https://br.xiapibuy.com/',
+        ];
+
+        $params = '';
+
+        //&maxPrice=50&minPrice=10
+        $maxPrice = \Yii::$app->request->get('maxPrice');
+        $minPrice = \Yii::$app->request->get('minPrice');
+        if ($maxPrice > 0){
+            $params .= '&price_max='.$maxPrice;
+        }
+        if ($maxPrice > 0){
+            $params .= '&price_min='.$minPrice;
+        }
+
+        $itemBaseUrl = $urlList[\Yii::$app->request->get('store')]??'';
+        if (!$itemBaseUrl){
+            return JsonUtil::error('get url error');
+        }
 
         $keyword = urlencode(\Yii::$app->request->get('keyword'));
         $header = array('User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36');
-        $url = "https://shopee.com.my/api/v2/search_items/?by=sales&keyword={$keyword}&limit=100&newest=0&order=desc&page_type=search&version=2";
-        $res = $this->curlGet($url, $header);
+
+        if (\Yii::$app->request->get('type') == 1) {
+
+            $url = $itemBaseUrl."api/v2/search_items/?by=sales&keyword={$keyword}&limit=100&newest=0&order=desc&page_type=search&version=2".$params;
+            $res = $this->curlGet($url, $header);
+        }else{
+            $url = $itemBaseUrl."api/v4/shop/get_shop_detail?username=".$keyword;
+            $res = $this->curlGet($url, $header);
+            $res = json_decode($res, true);
+            sleep(1);
+            $url = $itemBaseUrl."api/v2/search_items/?by=sales&limit=100&match_id={$res['data']['shopid']}&newest=0&order=desc&page_type=shop&version=2".$params;
+            $res = $this->curlGet($url, $header);
+        }
+
         $arr = json_decode($res, true);
 
 //        echo '<pre>';
@@ -36,12 +77,18 @@ class MarketController extends BackendBaseController
 
         $totalSold = 0;
         $totalPrice = 0;
+        $realTotalPrice = 0;
         $goodsList = [];
+        if (!$arr['items']){
+            return JsonUtil::error('数据未获取到:'.print_r($arr, true));
+        }
         foreach ($arr['items'] as $k => $v){
             $totalSold += (int)$v['sold'];
             $totalPrice += (int)$v['price'];
+            $realTotalPrice += (int)$v['price']*(int)$v['sold'];
             //url，标题，价格，上架时间，天数，点赞数（平均），观看数（平均），历史销量（平均），最近销量，图片
             $days = (int)(time() - (int)$v['ctime'])/86400;
+            $days = $days <=0 ? 1 : $days;
             $imgUrl = 'https://cf.shopee.com.my/file/';
             $imgList = [
                 $imgUrl.$v['images'][0]
@@ -55,20 +102,25 @@ class MarketController extends BackendBaseController
                 'price' => ((int)$v['price'])/100000,
                 'ctime' => date('Y-m-d', $v['ctime']),
                 'days' => sprintf("%.2f",$days),
-                'liked_count' => $v['liked_count'].'(avg: '.sprintf("%.2f",(int)$v['liked_count']/$days).')',
-                'view_count' => $v['view_count'].'(avg: '.sprintf("%.2f",(int)$v['view_count']/$days).')',
-                'historical_sold' => $v['historical_sold'].'(avg: '.sprintf("%.2f",(int)$v['historical_sold']/$days).')',
+                'liked_count' => $v['liked_count'],
+                'liked_count_avg' => sprintf("%.2f",(int)$v['liked_count']/$days),
+                'view_count' => $v['view_count'],
+                'view_count_avg' => sprintf("%.2f",(int)$v['view_count']/$days),
+                'historical_sold' => $v['historical_sold'],
+                'historical_sold_avg' => sprintf("%.2f",(int)$v['historical_sold']/$days),
                 'sold' => $v['sold'],
                 'ads_keyword' => $v['ads_keyword'],
-                'images' => $imgList
+                'images' => $imgList,
+                'shop_location' => $v['shop_location'],
             ];
 
             $goodsList[] = $temp;
         }
 
-        $data['avgPrice'] = sprintf('%.2f', ($totalPrice/100000)/count($arr['items']));
-        $data['avgSold'] = sprintf('%.2f', $totalSold/$data['total_count']);
+        $data['avgPrice'] = count($arr['items']) > 0 ? sprintf('%.2f', ($totalPrice/100000)/count($arr['items'])) : 0;
+        $data['avgSold'] = $data['total_count'] > 0 ? sprintf('%.2f', $totalSold/$data['total_count']) : 0;
         $data['count_sold'] = $totalSold;
+        $data['realAvgPrice'] = $totalSold > 0 ? sprintf('%.2f', ($realTotalPrice/100000)/$totalSold) : 0;
 
 //        echo '<pre>';
 //        print_r($data);
