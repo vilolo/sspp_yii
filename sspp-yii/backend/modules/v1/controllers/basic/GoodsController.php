ab0ecd4a52d5b2e466996fbd65b52d973cd272c1
feng <123@qq.com>
2020-12-23 20:44:37 +0800

    udpate

--- sspp-yii/backend/modules/v1/controllers/basic/GoodsController.php
+++ sspp-yii/backend/modules/v1/controllers/basic/GoodsController.php
@@ -5,6 +5,7 @@ namespace backend\modules\v1\controllers\basic;
 
 
 use backend\modules\v1\controllers\base\BackendBaseController;
+use common\utils\JsonUtil;
 
 class GoodsController extends BackendBaseController
 {//http://demo.local/index.php/v1/basic/goods/collect
@@ -23,15 +24,19 @@ class GoodsController extends BackendBaseController
             'statistics' => \Yii::$app->request->post('statistics'),
             'platform' => \Yii::$app->request->post('platform'),
             'json_item' => \Yii::$app->request->post('json_item'),
+            'created_at' => date('Y-m-d H:i:s')
         ];
-        $res = \Yii::$app->db->createCommand()->insert('sign_log', $data)->execute();
-        return $res;
+        if (\Yii::$app->db->createCommand()->insert('sign_log', $data)->execute()){
+            return JsonUtil::success();
+        }else{
+            return JsonUtil::error('收藏失败');
+        }
     }
 
     public function actionShowLog()
     {
-        $res = \Yii::$app->getDb()->createCommand()
-        return $res;
+//        $res = \Yii::$app->db->createCommand()
+//        return $res;
     }
 
     public function actionShowCollect()
